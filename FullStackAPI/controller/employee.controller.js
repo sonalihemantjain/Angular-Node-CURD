5790abf9a78d963be0281f6630c80d89c6a5d637
Sonali Jain <sonali.jain@stacksports.com>
2023-12-19 14:06:43 +0530

    add store procedure

--- FullStackAPI/controller/employee.controller.js
+++ FullStackAPI/controller/employee.controller.js
@@ -15,6 +15,21 @@ class EmpController {
     }
   }
 
+
+  async getAllDataWithSp(req, res) {
+    try {
+      const pool = await poolPromise;
+      const result = await pool.request().query(queries.getAllDataWithSp); // Call stored procedure
+
+      res.json(result.recordset);
+    } catch (error) {
+      res.status(500);
+      res.send(error.message);
+    }
+  }
+
+
+
   async addNewEmp(req, res) {
     try {
       console.log("Variable value:", req.body);
--- FullStackAPI/routes/route.js
+++ FullStackAPI/routes/route.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.get('/api/getAllData', controller.getAllData);
 router.post('/api/addNewEmp', controller.addNewEmp);
 router.delete('/api/deleteEmp', controller.deleteEmp);
-router.delete('/api/updateEmp', controller.updateEmp);
-
+router.put('/api/updateEmp', controller.updateEmp);
+router.get('/api/getAllDataWithSp', controller.getAllDataWithSp);
 
 module.exports = router;
